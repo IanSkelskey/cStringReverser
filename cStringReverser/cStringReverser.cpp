439e92bf56a07e41b08636dd684829b720bbe904
Ian Skelskey <ianskelskey@gmail.com>
2020-02-03 01:21:04 -0500

    -transferring classwork

--- cStringReverser/cStringReverser.cpp
+++ cStringReverser/cStringReverser.cpp
@@ -1,20 +1,40 @@
-// cStringReverser.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+#include<cstring>
 
-#include <iostream>
+using namespace std;
+
+
+char cString[100], rcString[100];
+
+void reverse(char*);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Enter a string to be reversed: ";
+	cin.getline(cString, 100);
+	reverse(cString);
+	cout << "Your string backwards is: " << cString << endl;
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void reverse(char *string)
+{
+	int length, i;
+	char *head, *tail, temp;
+	length = strlen(cString);
+	head = string;
+	tail = string;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (i = 0; i < (length - 1); i++)
+		tail++;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = *tail;
+		*tail = *head;
+		*head = temp;
+		head++;
+		tail--;
+	}
+}
